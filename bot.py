d5f402bfb555758b8c67ab7878385a2eab0cfa01
Patrick Doran <wombatpatrick@gmail.com>
2020-03-05 16:14:19 -0800

    Fixed name display

--- bot.py
+++ bot.py
@@ -9,6 +9,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("f{client.user} has connected to Discord")
+    print(f'{client.user} has connected to Discord')
 
 client.run(token)
