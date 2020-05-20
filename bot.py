74e0630d0a8a1fdd2f9c023022f324e583928c6a
Patrick-Doran <wombatpatrick@gmail.com>
2020-05-19 18:23:34 -0700

    Added logging

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+import logging
 import os
 import discord  # pylint: disable=import-error
 from dotenv import load_dotenv  # pylint: disable=import-error
+
+#Implements logging into application
+logging.basicConfig(level=logging.INFO) #NOTE:bot takes more time to start up, disable for speedier startups
 
 #Gather private info from env
 load_dotenv()
@@ -25,5 +29,7 @@ async def on_message(message): #upon hearing the call of ~
         return
     if message.content.startswith('~hello'):
         await message.channel.send('Hello.')
+    else:
+        await message.channel.send('I do not know that one.')
 
 client.run(token)
